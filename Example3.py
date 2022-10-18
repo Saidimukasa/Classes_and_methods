137cb8af7e1f073f8c32344e9c3e24e658e1c4b7
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-10-18 20:06:13 +0300

    Update Example3.py

--- Example3.py
+++ Example3.py
@@ -4,10 +4,10 @@ def person():
     birth_date = 2002 # taking all users to have the same birthdate
     age = 2022-birth_date 
     if name == "": # if no name has been entered
-        print("\n name : name" ,"\n weight : ",weight, "\n birthdate : ",birth_date)
+        print("\n name : name" ,"\n weight : ",weight, "\n birthdate : ", birth_date)
         print("age" , age)
     else:
-        print("\n name : ",name ,"\n weight : ",weight, "\n birthdate : ",birth_date)
+        print("\n name : ",name ,"\n weight : ",weight, "\n birthdate : ", birth_date)
 
 def main():
     person() # void function
