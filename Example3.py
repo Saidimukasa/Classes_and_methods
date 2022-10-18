eaf82714a5657f2c3669f2871e8fcdbfbba61eeb
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-10-18 10:38:03 +0300

    Create Example3.py

---
+++ Example3.py
@@ -0,0 +1,14 @@
+def person():
+    name = input("Enter the name : ") #allows a user to enter the name
+    weight = float(input("Enter the weight : "))
+    birth_date = 2002 
+    age = 2022-birth_date 
+    if name == "":
+        print("\n name : name" ,"\n weight : ",weight, "\n birthdate : ",birth_date)
+        print("age" , age)
+    else:
+        print("\n name : ",name ,"\n weight : ",weight, "\n birthdate : ",birth_date)
+
+def main():
+    person()
+main()
