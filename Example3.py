830b0688b34bfd029a8f3dc84e87586c884b7d74
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-10-18 10:39:10 +0300

    Update Example3.py

--- Example3.py
+++ Example3.py
@@ -3,7 +3,7 @@ def person():
     weight = float(input("Enter the weight : "))
     birth_date = 2002 # taking all users to have the same birthdate
     age = 2022-birth_date 
-    if name == "":
+    if name == "": # if no name has been entered
         print("\n name : name" ,"\n weight : ",weight, "\n birthdate : ",birth_date)
         print("age" , age)
     else:
